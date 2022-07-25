fa479da32f45f1c5ee8725bf56e1a57b858cc5a6
DSLim <test@test.com>
2022-07-25 21:25:27 +0900

    Updates

---
+++ precache-manifest.4017a55b916351669d82fb40d3988686.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09fec048dc6822fc3a74e360ede79676",
+    "url": "/movie_app_v02/index.html"
+  },
+  {
+    "revision": "e54cd4c2f5a0c77d5502",
+    "url": "/movie_app_v02/static/css/main.78c6cf97.chunk.css"
+  },
+  {
+    "revision": "4414abf3ef18d4edf612",
+    "url": "/movie_app_v02/static/js/2.fd0c205d.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/movie_app_v02/static/js/2.fd0c205d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e54cd4c2f5a0c77d5502",
+    "url": "/movie_app_v02/static/js/main.936fe509.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/movie_app_v02/static/js/main.936fe509.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8287da89cab73440dff",
+    "url": "/movie_app_v02/static/js/runtime-main.f900c710.js"
+  }
+]);
